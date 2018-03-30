07679adf76a82d3de877030db703bceaf8817faa
Ray <ray@protozo.net>
2018-03-30 05:30:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,43 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+
+ var itemObject = {};
+
+ itemObject[`${itemName}`] = Math.floor(Math.random()*100+1);
+ cart.push(itemObject);
+ console.log(`${itemName} has been added to your cart.`);
+ return cart;
+
 }
 
-function viewCart() {
-  // write your code here
+function viewCart()
+{
+    var itemArr = [];
+    var itemValue = [];
+    if(cart.length === 0)
+    {
+        console.log("Your shopping cart is empty.");
+    }
+    else if(cart.length === 1)
+    {
+
+        itemArr.push(Object.getOwnPropertyNames(cart[0])[0]);
+        itemValue.push(Object.getOwnPropertyDescriptor(cart[0], itemArr[0]).value);
+        console.log(`In your cart, you have ${itemArr[0]} at $${itemValue[0]}.`);
+    }
+    else if(cart.length === 2)
+    {
+
+      for(var i = 0; i <= cart.length+1; i++)
+      {
+        itemArr.push(Object.getOwnPropertyNames(cart[i+1])[i+1]);
+        itemValue.push(Object.getOwnPropertyDescriptor(cart[i+1], itemArr[i+1]).value);
+      }
+      console.log(`In your cart, you have ${itemArr[0]} at $${itemValue[0]} and ${itemArr[1]} at $${itemValue[0]}.`);
+    }
+
 }
 
 function total() {
